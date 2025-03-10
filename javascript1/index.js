e1b0354a3d935a4582f1a3283392843e06fcd04b
Md. Nadim Hossain <rsnadim.pro5133@gmail.com>
2025-03-10 22:12:18 +0600

    Create index.js

---
+++ javascript1/index.js
@@ -0,0 +1,10 @@
+let userName,income,expenses;
+userName = prompt("My name is : ");
+
+if(userName!=null)
+{
+    alert("Welcome, "+userName+" !");
+}
+else{
+    alert("You escaped the input!");
+}
