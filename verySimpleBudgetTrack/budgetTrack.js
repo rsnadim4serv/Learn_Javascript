49b1abf9f9c4f21048885d4d11dd66b88d2a7ac6
Md. Nadim Hossain <rsnadim.pro5133@gmail.com>
2025-03-18 14:24:38 +0600

    Update budgetTrack.js

--- verySimpleBudgetTrack/budgetTrack.js
+++ verySimpleBudgetTrack/budgetTrack.js
@@ -23,6 +23,31 @@ let Balance=netIncome-totalExpenses;
 let savings=Balance*0.25;
 //Current Balance after savings
 let curBalance=Balance-savings;
+
+if(savings>=1000){
+    financialStatus='Excellent, savings so well!';
+}
+else if(savings>=500 && savings<1000)
+{
+    financialStatus='Good, savings are quite good';
+}
+else if(savings>=100 && savings<500)
+{
+    financialStatus='Need improvement, try your best';
+}
+else if(savings<100)
+{
+    financialStatus='Critical, give more and more hard work';
+}
+//let saving=450;
+//let financial = saving>=1000 ? 'Excellent' : saving>=500 && saving<1000 ? 'Good' : saving>=100 && saving<500 ? 'Need Improvement' : saving<100 ? 'Critical' : 'Warning';
+//console.log(financial);
+let overSpending='';
+if(totalExpenses>totalIncome)
+{
+    overSpending='Warning, expenses are greater than total income';
+}
+
 //Show My Wallet Report
 console.log('Total Income: $'+totalIncome);
 console.log('Tax deduction(12% of totalIncome): $'+tax);
@@ -31,3 +56,8 @@ console.log('Total Expenses: $'+totalExpenses);
 console.log('Current Balance after expenses: $'+Balance)
 console.log('Savings : $'+savings);
 console.log('Current balance after savings : $'+curBalance);
+console.log('\nFinancial Status\n'+financialStatus);
+if(overSpending)
+{
+    console.log(overSpending);
+}
